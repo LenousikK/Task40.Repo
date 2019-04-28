3682144be59294565b33ca42a17a2a73ee9bdfd8
LenousikK <liloo-19@yandex.ru>
2019-04-28 15:38:22 +0300

    Added: 6.Create 3 tests for alerts (URL - https://the-internet.herokuapp.com/javascript_alerts). (Done) 1) Small corrections (re-naming of the test method) in test class: TestConfirmAlert; 2) Added test class: TestPromptAlert.

--- src/test/java/TestPromptAlert.java
+++ src/test/java/TestPromptAlert.java
@@ -7,10 +7,11 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
-public class TestConfirmAlert {
+public class TestPromptAlert {
     private static final String URL = "https://the-internet.herokuapp.com/javascript_alerts";
     private static final String TITLE = "The Internet";
-    private static final By BUTTON_OPEN_ALERT = By.cssSelector("button[onclick= 'jsConfirm()']");
+    private static final By BUTTON_OPEN_ALERT = By.cssSelector("button[onclick= 'jsPrompt()']");
+    private static final String TEXT_TO_ENTER = "Some text";
     private static final By RESULT_MESSAGE = By.cssSelector("p#result");
     private WebDriver driver;
     WebDriverWait wait;
@@ -29,14 +30,15 @@ public class TestConfirmAlert {
     }
 
     @Test
-    public void ConfirmAlert() {
+    public void EnterValueInPromptAlert() {
         wait.until(titleIs(TITLE));
         driver.findElement(BUTTON_OPEN_ALERT).click();
         try {
             Alert alert = driver.switchTo().alert();
+            alert.sendKeys(TEXT_TO_ENTER);
             alert.accept();
             WebElement resultMessage = driver.findElement(RESULT_MESSAGE);
-            assertEquals("You clicked: Ok", resultMessage.getText());
+            assertEquals("You entered: " + TEXT_TO_ENTER, resultMessage.getText());
         } catch (NoAlertPresentException e) {
             e.printStackTrace();
         }
